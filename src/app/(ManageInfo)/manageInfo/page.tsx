080df43a37d12487b057cc1f6726c12c452f97ee
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-11-12 13:40:36 +0300

    Initial commit

---
+++ src/app/(ManageInfo)/manageInfo/page.tsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import InputWithLabel from '@/components/InputWithLabel';
+import { Button } from '@nextui-org/react';     
+
+const ManageInfo = () => {
+  return (
+    <div className="min-h-[1117px] flex flex-col items-center justify-between"> 
+      {/* Include Header */}
+      <Header />
+
+      <hr className="w-full border-t-1 border-[rgb(207,212,218)]  py-5 "/>
+
+      {/* Form Container */}
+      <div className="flex flex-col items-center justify-start w-[428px] h-[293px] gap-[40px] mt-10 mb-10 flex-1">
+
+        <div className='flex flex-cols items-center justify-center w-[1728px] h-[108px] border'>
+            <h2 className="w-[428px] h-[34px] text-center text-[24px] font-semibold text-black">마이페이지</h2>
+
+            <div className='flex flex-row items-center justify-between gap-[60px]'>
+
+                <div className='border-b-3 border-[#42A8FD] text-[#42A8FD] pb-3 '>내 정보 관리</div>
+                <div className='text-[#868F9A]'>이용권 및 결제 내역</div>
+                <div className='text-[#868F9A]'>다운로드 내역</div>
+                <div className='text-[#868F9A]'>나의 활동</div>
+                
+            </div>
+        </div>
+
+        
+
+        
+        <div className="flex flex-col items-center justify-center gap-[40px]">
+            <div className="flex flex-col items-center justify-center gap-[12px] w-[428px]">
+                <InputWithLabel
+                    label="아이디"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="abc123"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+                />
+
+                <InputWithLabel
+                    label="비밀번호"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="6자리 이상 영문/숫자 조합"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+                />
+
+                <InputWithLabel
+                    label="비밀번호 확인"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="6자리 이상 영문/숫자 조합"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+                />
+
+                <InputWithLabel
+                    label="이메일 주소"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="abc@aaa.com"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+                />
+
+                <InputWithLabel
+                    label="이름"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="홍길동"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+                />
+
+                <InputWithLabel
+                    label="휴대폰 번호"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="010-1111-2222"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+
+                />
+
+                <InputWithLabel
+                    label="생년월일"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="2008.01.01"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+
+                />
+
+                <InputWithLabel
+                    label="성별"
+                    labelStyles="font-semibold w-[105px] h-[22] text-[16px]"
+                    mainStyles="flex w-[428px] justify-between items-center"
+                    placeholder="여성"
+                    inputParentStyles="w-[323px] h-[40px]"
+                    type="text"
+                    disabled={true}
+                />
+            </div>
+
+
+                {/* Submit Button */}
+                <Button className="w-[428px] h-[42px] px-[20px] py-[10px] bg-[#42A8FD] hover:bg-[#489de2] font-semibold text-white rounded-md">
+                        가입하기
+                </Button>
+
+                <p>탈퇴하기</p>
+        </div>
+        
+      </div>
+
+      {/* Footer remains at the bottom of the page */}
+      <Footer />
+    </div>
+  )
+}
+
+export default ManageInfo
