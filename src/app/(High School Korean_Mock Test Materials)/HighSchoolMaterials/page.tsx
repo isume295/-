1535fa4449eba8de01cd9d7802c948dfba1d4298
Muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-11-14 11:31:51 +0300

    initial commmit

---
+++ src/app/(High School Korean_Mock Test Materials)/HighSchoolMaterials/page.tsx
@@ -0,0 +1,221 @@
+'use client';
+
+import {useState, useMemo} from 'react'
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Sidebar from '@/components/Sidebar';
+import { Checkbox } from "@nextui-org/react";
+import {PaymentData}  from '@/assets/paymentDataT1';
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+    Pagination,
+  } from "@nextui-org/react";
+    
+
+const HighSchoolMaterials = () => {
+      // Pagination Logic
+        const [page, setPage] = useState(1);
+
+        const rowsPerPage = 10
+
+        const pages = Math.ceil(PaymentData.length / rowsPerPage);
+
+        const [currentData, setCurrentData] = useState<any>();
+
+        const items = useMemo(() => {
+            const start = (page - 1) * rowsPerPage;
+            const end = start + rowsPerPage;
+
+            setCurrentData(PaymentData.slice(start, end));
+            return PaymentData.slice(start, end);
+        }, [page, PaymentData, 10, rowsPerPage]);
+
+      
+  return (
+    <div className="min-h-[2949px] flex flex-col items-center justify-between">
+        <Header />
+        <hr className="w-full border-t-1 border-[rgb(207,212,218)]" />
+
+
+        {/* Sidebar and the right side */}
+        <div className='flex flex-row  justify-between w-[1080px] gap-[36px]'>
+            {/* SideBar */}
+            <Sidebar />
+
+            {/* RightSide */}
+            <div className='flex flex-col items-center justify-center w-[824px] h-[1191px] py-[60px] gap-[40px]'>
+                {/* Top  */}
+                <div className='flex flex-col items-center justify-center w-[824px] h-[266px] gap-[12px]'>
+                    <div className='flex flex-col items-center justify-center w-[824px] h-[34px] '>
+                        <h3 className='font-bold text-[24px]'>고등국어(모의고사 자료)</h3>
+                    </div>
+
+                    <div className='flex flex-col items-center justify-center border w-[516px] h-[220px] '>
+                        {/* A row  */}
+                        <div className='w-[516px] h-[44px] border-b border-[#CFD4DA] flex flex-row items-start justify-start'>
+                            <div className='flex flex-row items-start justify-start w-[85px] h-[44px] p-[12px] border-b border-[#CFD4DA] bg-[#F3F4F6] gap-[4px]'>
+                                <div className='flex flex-row items-center justify-between'>
+                                        <Checkbox  size='sm' />
+                                        <p className='w-[42px] h-[17px] text-[10px] text-[#868F9A]'> 학년 </p>
+                                </div>
+                            </div>
+                            <div className='flex flex-row items-start justify-start w-[431px] h-[44px] p-[8px] gap-[4px]'>
+                                <p className='text-[12px] rounded-full py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>고1</p>
+                                <p className='text-[12px] rounded-full py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>고2</p>
+                                <p className='text-[12px] rounded-full py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>고3</p>
+
+                            </div>
+                        </div>
+                        {/* A row  */}
+                        <div className='w-[516px] h-[44px] border-b border-[#CFD4DA] flex flex-row items-start justify-start'>
+                            <div className='flex flex-row items-start justify-start w-[85px] h-[44px] p-[11px] border-b border-[#CFD4DA] bg-[#F3F4F6] gap-[2px]'>
+                                    <div className='flex flex-row items-center justify-between'>
+                                        <Checkbox  size='sm' />
+                                        <p className='w-[42px] h-[17px] text-[10px] text-[#868F9A]'> 시행연도 </p>
+                                    </div>
+                                    
+
+                            </div>
+                            <div className='flex flex-row items-start justify-start w-[431px] h-[44px] p-[8px] gap-[4px]'>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>2025</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>2024</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>2023</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>2022</p>
+
+
+                            </div>
+                        </div>
+                        {/* A row  */}
+                        <div className='w-[516px] h-[44px] border-b border-[#CFD4DA] flex flex-row items-start justify-start'>
+                            <div className='flex flex-row items-start justify-start w-[85px] h-[44px] p-[12px] border-b border-[#CFD4DA] bg-[#F3F4F6] gap-[4px]'>
+                                <div className='flex flex-row items-center justify-between'>
+                                        <Checkbox  size='sm' />
+                                        <p className='w-[42px] h-[17px] text-[10px] text-[#868F9A]'> 월 </p>
+                                </div>   
+                            </div>
+                            <div className='flex flex-row items-start justify-start w-[431px] h-[44px] p-[8px] gap-[4px]'>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>3월</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>4월</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>6월</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>7월</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>9월</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>10월</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>11월</p>
+
+
+
+                            </div>
+                        </div>
+                        {/* A row  */}
+                        <div className='w-[516px] h-[44px] border-b border-[#CFD4DA] flex flex-row items-start justify-start'>
+                            <div className='flex flex-row items-start justify-start w-[85px] h-[44px] p-[12px] border-b border-[#CFD4DA] bg-[#F3F4F6] gap-[4px]'>
+                                <div className='flex flex-row items-center justify-between'>
+                                        <Checkbox  size='sm' />
+                                        <p className='w-[42px] h-[17px] text-[10px] text-[#868F9A]'> 분야 </p>
+                                </div>
+                            </div>
+                            <div className='flex flex-row items-start justify-start w-[431px] h-[44px] p-[8px] gap-[4px]'>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>독서</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>문학</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>화법과 작문</p>
+                                <p className='text-[12px] py-[4px] px-[8px] gap-[10px] text-[#868F9A]'>언어와 매체(문법)</p>
+
+
+                            </div>
+                        </div>
+                        {/* A row  */}
+                        <div className='w-[516px] h-[44px] border-b border-[#CFD4DA] flex flex-row items-start justify-start'>
+                            <div className='flex flex-row items-start justify-start w-[85px] h-[44px] p-[12px] border-b border-[#CFD4DA] bg-[#F3F4F6] gap-[4px]'>
+                                <div className='flex flex-row items-center justify-between'>
+                                        <Checkbox  size='sm' />
+                                        <p className='w-[42px] h-[17px] text-[10px] text-[#868F9A]'> 유형 </p>
+                                </div>
+                            </div>
+                            <div className='flex flex-row items-start justify-start w-[431px] h-[44px] p-[8px] gap-[4px]'>
+                                <p className='text-[12px] py-[4px] px-[8px] text-[#868F9A]'>해설・정리</p>
+                                <p className='text-[12px] py-[4px] px-[8px] text-[#868F9A]'>연관 문제</p>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+                {/* Bottom  */}
+                <div className='w-[824px] h-[765px] gap-[24px]'>
+                    <article className="mt-5">
+                            <Table
+                                aria-label="Data Table"
+                                shadow="none"
+                                classNames={{
+                                th: [
+                                    "font-normal text-[16px] bg-[#EEEEEE] text-[#A1A9A3] h-[48px]  text-center",
+                                ],
+                                td: ["px-6  text-center font-normal text-base text-[#363941] "],
+                                }}
+                                bottomContent={
+                                <div className="flex w-full justify-center  mt-8">
+                                    <Pagination
+                                    isCompact
+                                    showControls
+                                    showShadow
+                                    color="secondary"
+                                    page={page}
+                                    total={pages}
+                                    onChange={(page) => setPage(page)}
+                                    />
+                                </div>
+                                }
+                            >
+                                <TableHeader>
+                                
+
+                                <TableColumn>번호</TableColumn>
+                                <TableColumn>닉네임</TableColumn>
+                                <TableColumn>휴대폰번호</TableColumn>
+                                <TableColumn>로그인</TableColumn>
+                                <TableColumn>가입일</TableColumn>
+                                {/* <TableColumn>상세보기</TableColumn> */}
+                                </TableHeader>
+                                <TableBody>
+                                {items.map((row) => (
+                                    <TableRow key={row.id} className="border-b-1">
+                                    <TableCell>{row.number}</TableCell>
+                                    <TableCell>{row.nickname}</TableCell>
+                                    <TableCell>{row.phoneNumber}</TableCell>
+                                    <TableCell>{row.logIn}</TableCell>
+                                    <TableCell>{row.joinDate}</TableCell>
+                                    {/* <TableCell>
+                                        <Link
+                                        href="/admin/membership/membership-management/1"
+                                        className="text-mainPurple underline underline-offset-2"
+                                        >
+                                        {row.viewDetails}
+                                        </Link>
+                                    </TableCell> */}
+                                    </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                    </article>
+
+
+                </div>
+            </div>
+        </div>
+
+
+        <Footer />
+      
+    </div>
+  )
+}
+
+export default HighSchoolMaterials;
---
+++ src/app/(MyActivities)/myActivities/page.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import { Button, Card, CardBody } from '@nextui-org/react';    
+import Image from 'next/image'; 
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Pagination, getKeyValue} from "@nextui-org/react";
+import { PaymentData } from "@/assets/paymentDataT1";
+
+const MyActivities = () => {
+    const [page, setPage] = React.useState(1);
+    const rowsPerPage = 10;
+
+    const pages = Math.ceil(PaymentData.length / rowsPerPage);
+
+    const items = React.useMemo(() => {
+        const start = (page - 1) * rowsPerPage;
+        const end = start + rowsPerPage;
+
+        return PaymentData.slice(start, end);
+    }, [page, PaymentData]);
+
+  return (
+    <div className="min-h-[1117px] flex flex-col items-center justify-between"> 
+      {/* Include Header */}
+      <Header />
+
+      <hr className="w-full border-t-1 border-[rgb(207,212,218)]  py-2"/>
+
+      {/* Form Container */}
+      <div className="flex flex-col items-center justify-start w-[428px] h-[293px] gap-[10px] mt-10 mb-10 flex-1">
+            <h2 className="w-[428px] h-[34px] text-center text-[24px] font-semibold text-black">마이페이지</h2>
+
+            {/* ----- ---- --- TOP  --- ----- ------*/}
+            <div className='flex flex-cols items-center justify-center w-[1728px] h-[108px]'>
+                <div className="w-full flex justify-center border-b border-[rgb(207,212,218)]">
+                    {/* Inner content container */}
+                    <div className="flex flex-row items-center justify-between gap-[60px]">
+                        <div className="text-[#868F9A]  pb-3 font-bold">내 정보 관리</div>
+                        <div className="text-[#868F9A] pb-3 font-bold">이용권 및 결제 내역</div>
+                        <div className="text-[#868F9A] pb-3 font-bold">다운로드 내역</div>
+                        <div className=" border-b-4 border-[#42A8FD] text-[#42A8FD] pb-3 font-bold">나의 활동</div> 
+                    </div>
+                </div>
+                
+            </div>
+
+        
+
+            {/* ----- ---- --- BOTTOM  --- ----- ------*/}
+            <div className='flex flex-col items-center justify-center w-[1080px] h-[582px] gap-[8px]'>
+
+                <div className="flex flex-row items-center justify-center w-full gap-[12px]">
+                    <div className=' border border-[#42A8FD] bg-blue-100 font-bold text-[#42A8FD] px-[12px] py-[10px] text-[14px] rounded-full'>YTWFqsFWQ</div>
+                    <div className=' border border-[#868F9A]  text-[#868F9A] px-[12px] py-[10px] text-[14px] rounded-full'>YTWFqsFWQ</div>
+                </div>
+
+                <div className='w-[1080px] h-[484px]'>
+                    <Table 
+                    aria-label="Example table with client side pagination"
+                    bottomContent={
+                    <div className="flex w-full justify-center">
+                        <Pagination
+                        isCompact
+                        showControls
+                        showShadow
+                        color="default"
+                        page={page}
+                        total={pages}
+                        onChange={(page) => setPage(page)}
+                        />
+                    </div>
+                    }
+                    classNames={{
+                    wrapper: " min-h-[222px] shadow-none rounded-none",
+                    }}
+                >
+                    <TableHeader>
+                    <TableColumn key="name"   className='text-center'>NAME</TableColumn>
+                    <TableColumn key="role"   className='text-center'>ROLE</TableColumn>
+                    <TableColumn key="status" className='text-center'>STATUS</TableColumn>
+                    <TableColumn key="status" className='text-center'>AGE</TableColumn>
+
+                    </TableHeader>
+                    <TableBody className='items-center justify-center border-b' items={items}>
+                    {(item) => (
+                        <TableRow key={item.name}>
+                        {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                        </TableRow>
+                    )}
+                    </TableBody>
+                </Table>
+                </div>
+                </div>
+
+
+        
+       </div>
+
+      {/* Footer remains at the bottom of the page */}
+      <Footer />
+    </div>
+  )
+}
+
+export default MyActivities;
+
