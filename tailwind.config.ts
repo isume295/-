f3a56420e40067df1295047d6a397632cead38f8
muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-11-21 17:12:48 +0300

    notyet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    
     extend: {
       fontFamily: {
         noto: ["Noto Sans KR", "sans-serif"],
@@ -27,6 +34,8 @@ const config: Config = {
         mainRed: "#F05858",
       },
     },
+
+    
   },
   darkMode: "class",
   plugins: [nextui()],
